7c5c906c18de3c28c34dbac50ae35219cc4ca7f9
Dustin Brink <dustinbrink@gmail.com>
2018-01-22 15:34:35 -0700

    update Procfile to start the correct process

--- server.js
+++ server.js
@@ -1,27 +1,13 @@
 // server.js
 
-// import http from 'http';
-// import express from 'express';
+const express = require('express');
+const path = require('path');
 
-// const path = require('path');
-// const serveStatic = require('serve-static');
-
-// const app = express();
-// app.use(serveStatic(path.join(__dirname, '/dist')));
-
-// const port = process.env.PORT || 5000;
-// app.listen(port);
-
-// console.log(`server started ${port}`);
-
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-//const serveStatic = require('serve-static')
+const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'dist')))
-  //.set('views', path.join(__dirname, 'views'))
-  //.set('view engine', 'ejs')
-  //.get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+  .use(express.static(path.join(__dirname, 'dist'))) // Serve static from /dist folder
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
+  // .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
